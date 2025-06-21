9f3d820b2952fd23dfa7e29cb4dc5694db2ddfc7
ab_mahin <mahin.cse7.bu@gmail.com>
2025-06-21 18:45:41 +0600

    Add DbClass

---
+++ MyAspNetProject/Migrations/20250621120321_InitialTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAspNetProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "varchar(100)", nullable: false),
+                    EmployeeGender = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    EmployeeDesignation = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    EmployeeMarried = table.Column<string>(type: "varchar(10)", nullable: false),
+                    EmployeeDescription = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- MyAspNetProject/Models/Employee.cs
+++ MyAspNetProject/Models/Employee.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MyAspNetProject.Models;
 
 public class Employee
 {
+    [Key] 
+    public int Id { get; set; }
+    [Column("EmployeeName", TypeName = "varchar(100)")]
     public string Name { get; set; }
+    [Column("EmployeeGender", TypeName = "varchar(10)")]
+
     public Gender Gender { get; set; }
     public int Age { get; set; }
+    [Column("EmployeeDesignation", TypeName = "varchar(200)")]
     public string Designation { get; set; }
     public int Salary { get; set; }
+    [Column("EmployeeMarried", TypeName = "varchar(10)")]
     public string Married { get; set; }
+    [Column("EmployeeDescription", TypeName = "varchar(200)")]
     public string Description { get; set; }
 }
 
