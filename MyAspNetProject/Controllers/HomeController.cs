318257d20d47334d2c30f13194d9f47f726d1c4b
ab_mahin <mahin.cse7.bu@gmail.com>
2025-06-21 23:23:35 +0600

    add

--- MyAspNetProject/Controllers/HomeController.cs
+++ MyAspNetProject/Controllers/HomeController.cs
@@ -6,11 +6,17 @@ namespace MyAspNetProject.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly EmployeeDbContext _employeeDb;
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+    }
+
+    public HomeController(EmployeeDbContext employeeDb)
+    {
+        _employeeDb = employeeDb;
     }
 
     public IActionResult Index()
